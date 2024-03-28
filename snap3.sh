1615881dfdf685c2edff910230a596efdd7f61e6
iAmTheRealJason <51766718+JasonGardner-code@users.noreply.github.com>
2024-03-27 20:12:25 -0700

    Update snap3.sh

---
+++ snap3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create a new HTML file
+echo "<html><body>" > subdomains.html
+
+# Loop through each subdomain in the text file
+while IFS= read -r subdomain; do
+    # Use wget to capture a screenshot of the front page of each subdomain with a timeout of 10 seconds
+    timeout 10 wget -q "http://$subdomain" -O "$subdomain.png"
+    
+    # Add the screenshot to the HTML file
+    echo "<h2>$subdomain</h2>" >> subdomains.html
+    echo "<img src='$subdomain.png'>" >> subdomains.html
+
+    # Remove the screenshot file
+    rm "$subdomain.png"
+done < subdomains.txt
+
+# Close the HTML file
+echo "</body></html>" >> subdomains.html
+
+echo "Snapshot of each subdomain's front page created in subdomains.html"
